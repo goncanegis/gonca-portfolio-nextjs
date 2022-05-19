f637cac78e4ddbc49b3393cbc92662da9fbd7b02
Gonca Negis <gonca.negis@gmail.com>
2022-05-19 23:28:24 +0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import { Navbar } from './components/Navbar';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <Navbar />
+      <header>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </header>
-    </div>
+    </>
   );
 }
 
