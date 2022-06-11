0373ba9cc864b4138146c00628935331801d4fef
Gonca Negis <gonca.negis@gmail.com>
2022-06-11 14:23:59 +0300

    added service worker

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,36 @@ import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/layout';
 import theme from '../theme';
+import { useEffect } from 'react';
+import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(
+          function (registration) {
+            console.log(
+              'Service Worker registration successful with scope: ',
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log('Service Worker registration failed: ', err);
+          }
+        );
+      });
+    }
+  }, []);
+
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Layout>
         <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <title>Gonca Negis</title>
         </Head>
         <Component {...pageProps} />
